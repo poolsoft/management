6656fcc3860bb8565862d4f80fef47edbb90ed1d
hluffy <18801613171@163.com>
2017-02-22 17:17:14 +0800

    add get focus things

--- src/main/webapp/js/cookie.js
+++ src/main/webapp/js/cookie.js
@@ -77,17 +77,17 @@ function SetCookie(name,value)//两个参数，一个是cookie的名子，一个
 
 }
 
-function getCookie(name)//取cookies函数        
-
-{
-
-    var arr = document.cookie.match(new RegExp("(^| )"+name+"=([^;]*)(;|$)"));
-
-     if(arr != null) return unescape(arr[2]); return null;
-
- 
-
-}
+//function getCookie(name)//取cookies函数        
+//
+//{
+//
+//    var arr = document.cookie.match(new RegExp("(^| )"+name+"=([^;]*)(;|$)"));
+//
+//     if(arr != null) return unescape(arr[2]); return null;
+//
+// 
+//
+//}
 
 function delCookie(name)//删除cookie
 {
